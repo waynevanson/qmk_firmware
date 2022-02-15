69555d7b1090abca149a0f5a9bbe4892db91322c
Wayne Van Son <waynevanson@gmail.com>
2022-02-16 09:33:11 +1100

    fix: move mods up a row

--- keyboards/moonlander/keymaps/waynevanson/config.h
+++ keyboards/moonlander/keymaps/waynevanson/config.h
@@ -36,8 +36,8 @@
 LAYOUT_moonlander(\
     XXX, XXX, XXX, XXX, XXX, XXX, XXX,        XXX, XXX, XXX, XXX, XXX, XXX, XXX,\
     XXX, XXX, K01, K02, K03, K04, XXX,        XXX, K05, K06, K07, K08, XXX, XXX,\
-    XXX, K00, K11, K12, K13, K14, XXX,        XXX, K15, K16, K17, K18, K09, XXX,\
-    K30, K10, K21, K22, K23, K24,                  K25, K26, K27, K28, K19, K37,\
-    K31, K20, XXX, XXX, XXX,      XXX,        XXX,      XXX, XXX, XXX, K29, K36,\
+    K30, K00, K11, K12, K13, K14, XXX,        XXX, K15, K16, K17, K18, K09, K37,\
+    K31, K10, K21, K22, K23, K24,                  K25, K26, K27, K28, K19, K37,\
+    XXX, K20, XXX, XXX, XXX,      XXX,        XXX,      XXX, XXX, XXX, K29, XXX,\
                         K32, K33, XXX,        XXX, K34, K35\
 )
