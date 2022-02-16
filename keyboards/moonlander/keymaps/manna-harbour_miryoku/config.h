5376b5953b5d280cd63da02512b25b2ed5be135d
Wayne Van Son <waynevanson@gmail.com>
2022-02-17 09:59:32 +1100

    feat: add workman to miryoku

--- keyboards/moonlander/keymaps/manna-harbour_miryoku/config.h
+++ keyboards/moonlander/keymaps/manna-harbour_miryoku/config.h
@@ -22,3 +22,5 @@ XXX, K20, K21, K22, K23, K24,               K25, K26, K27, K28, K29, XXX,\
 XXX, XXX, XXX, XXX, K32,      XXX,     XXX,      K37, XXX, XXX, XXX, XXX,\
                     K33, K34, XXX,     XXX, K35, K36\
 )
+
+#define MIRYOKU_ALPHAS_WORKMAN WORKMAN
