d772d053abf6cadcf8ef4922115acbf2b38c5ebf
Wayne Van Son <=>
2024-07-28 10:43:01 +1000

    chore: reformat

--- keyboards/bastardkb/charybdis/3x6/keymaps/waynevanson/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/waynevanson/keymap.c
@@ -57,54 +57,218 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define PT_Z LT(LAYER_POINTER, KC_Z)
 #define PT_SLSH LT(LAYER_POINTER, KC_SLSH)
 
+// TOP_LEFT,
+// TOP_RIGHT, 
+// MID_LEFT,
+// MID_RIGHT,
+// BOT_LEFT,
+// BOT_RIGHT,
+// THUMB_LEFT
+// THUMB_RIGHT
+//
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT(
-  // ╭──────────────────────────────────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────────────────────────────────╮
-       KC_LGUI, KC_Q,           KC_D,           KC_R,           KC_W,           KC_B,       KC_J,   KC_F,           KC_U,           KC_P,           KC_SCLN,        KC_RGUI,
-  // ├──────────────────────────────────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────────────────────────────────┤
-       KC_LCTL, LGUI_T(KC_A),   LALT_T(KC_S),   LCTL_T(KC_H),   LSFT_T(KC_T),   KC_G,       KC_Y,   RSFT_T(KC_N),   RCTL_T(KC_E),   RALT_T(KC_O),   RGUI_T(KC_I),   KC_RCTL,
-  // ├──────────────────────────────────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────────────────────────────────┤
-       KC_LSFT, KC_Z,           KC_X,           KC_M,           KC_C,           KC_V,       KC_K, KC_L,  LT(LAYER_POINTER, KC_QCOM),       KC_EDOT,         KC_QUOTE,       KC_RSFT,
-  // ╰──────────────────────────────────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────────────────────────────────╯
-                                                KC_ESC,         KC_SPC,         KC_TAB,     LT(FUNC, KC_ENT), LT(NMSY, KC_BSPC)
-  //                                          ╰─────────────────────────────────────────╯ ╰───────────────────────────╯
+       KC_LGUI,
+       KC_Q,
+       KC_D,
+       KC_R,
+       KC_W,
+       KC_B,
+       
+       KC_J,
+       KC_F,
+       KC_U,
+       KC_P,
+       KC_SCLN,
+       KC_RGUI,
+
+       KC_LCTL
+       LGUI_T(KC_A),
+       LALT_T(KC_S),
+       LCTL_T(KC_H),
+       LSFT_T(KC_T),
+       KC_G,
+       KC_Y,
+       RSFT_T(KC_N),
+       RCTL_T(KC_E),
+       RALT_T(KC_O),
+       RGUI_T(KC_I),
+       KC_RCTL,
+
+       KC_LSFT,
+       KC_Z,
+       KC_X,
+       KC_M,
+       KC_C,
+       KC_V,
+
+       KC_K,
+       KC_L,
+       LT(LAYER_POINTER, KC_QCOM),
+       KC_EDOT,
+       KC_QUOTE,
+       KC_RSFT,
+
+       KC_ESC,
+       KC_SPC,
+       KC_TAB,
+
+       LT(FUNC, KC_ENT),
+       LT(NMSY, KC_BSPC)
   ),
 
   [NMSY] = LAYOUT(
-  // ╭──────────────────────────────────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────────────────────────────────╮
-        KC_NO,  KC_LBRC,        KC_7,           KC_8,           KC_9,           KC_RBRC,    KC_NO,  KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-  // ├──────────────────────────────────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────────────────────────────────┤
-        KC_NO,  KC_SEMICOLON,   KC_4,           KC_5,           KC_6,           KC_EQL,     KC_NO,  KC_RSFT,        KC_RCTL,        KC_RALT,        KC_RGUI,        KC_NO,
-  // ├──────────────────────────────────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────────────────────────────────┤
-        KC_NO,  KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_BSLS,    KC_NO,  KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-  // ╰──────────────────────────────────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────────────────────────────────╯
-                                                KC_DOT,         KC_0,           KC_MINS,    KC_NO,  KC_NO
-  //                                          ╰─────────────────────────────────────────╯ ╰───────────────────────────╯
+        KC_NO,
+        KC_LBRC,
+        KC_7,
+        KC_8,
+        KC_9,
+        KC_RBRC,
+
+        KC_NO,
+        KC_NO,
+        KC_NO,
+        KC_NO,
+        KC_NO,
+        KC_NO,
+
+        KC_NO,
+        KC_SEMICOLON,
+        KC_4,
+        KC_5,
+        KC_6,
+        KC_EQL,
+
+        KC_NO,
+        KC_RSFT,
+        KC_RCTL,
+        KC_RALT,
+        KC_RGUI,
+        KC_NO,
+
+        KC_NO,
+        KC_GRAVE,
+        KC_1,
+        KC_2,
+        KC_3,
+        KC_BSLS,
+
+        KC_NO,
+        KC_NO,
+        KC_NO,
+        KC_NO,
+        KC_NO,
+        KC_NO,
+
+        KC_DOT,
+        KC_0,
+        KC_MINS,
+
+        KC_NO,
+        KC_NO
   ),
 
   [FUNC] = LAYOUT(
-  // ╭──────────────────────────────────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────────────────────────────────╮
-        KC_NO,  KC_F12,             KC_F7,              KC_F8,  KC_F9,  KC_PRINT_SCREEN,KC_NO,  KC_NO,              KC_NO,              KC_NO,  KC_NO,  KC_NO,
-  // ├──────────────────────────────────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────────────────────────────────┤
-        KC_NO,  KC_F11,             KC_F4,              KC_F5,          KC_F6, KC_SCROLL_LOCK,KC_NO,  KC_RIGHT_SHIFT, KC_RIGHT_CTRL,  KC_RIGHT_ALT,   KC_RIGHT_GUI, KC_NO,
-  // ├──────────────────────────────────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────────────────────────────────┤
-        KC_NO, KC_F11, KC_F1, KC_F2, KC_F3, KC_PAUSE, KC_NO,  KC_NO,              KC_NO,              KC_NO,  KC_NO,  KC_NO,
-  // ╰──────────────────────────────────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────────────────────────────────╯
-        KC_MENU, KC_SPACE, KC_TAB, KC_NO, KC_NO
-  //                                          ╰─────────────────────────────────────────╯ ╰───────────────────────────╯
+        KC_NO,
+        KC_F12,
+        KC_F7,
+        KC_F8,
+        KC_F9,
+        KC_PRINT_SCREEN,
+
+        KC_NO,
+        KC_NO,
+        KC_NO,
+        KC_NO,
+        KC_NO,
+        KC_NO,
+
+        KC_NO,
+        KC_F11,
+        KC_F4,
+        KC_F5,
+        KC_F6,
+        KC_SCROLL_LOCK,
+
+        KC_NO,
+        KC_RIGHT_SHIFT,
+        KC_RIGHT_CTRL,
+        KC_RIGHT_ALT,
+        KC_RIGHT_GUI,
+        KC_NO,
+
+        KC_NO,
+        KC_F11,
+        KC_F1,
+        KC_F2,
+        KC_F3,
+        KC_PAUSE,
+
+        KC_NO,
+        KC_NO,
+        KC_NO,
+        KC_NO,
+        KC_NO,
+        KC_NO,
+
+        KC_MENU,
+        KC_SPACE,
+        KC_TAB,
+
+        KC_NO,
+        KC_NO
   ),
 
   [LAYER_POINTER] = LAYOUT(
-  // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DPI_MOD, S_D_MOD,    S_D_MOD, DPI_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX,
-  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX, DRGSCRL,SNIPING,_______, EE_CLR,  QK_BOOT,    QK_BOOT, EE_CLR, _______, SNIPING, DRGSCRL, XXXXXXX,
-  // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  KC_BTN2, KC_BTN1, KC_BTN3,    KC_BTN3, KC_BTN1
-  //                            ╰───────────────────────────╯ ╰──────────────────╯
+        XXXXXXX,
+        XXXXXXX,
+        XXXXXXX,
+        XXXXXXX,
+        DPI_MOD,
+        S_D_MOD,
+
+        S_D_MOD,
+        DPI_MOD,
+        XXXXXXX,
+        XXXXXXX,
+        XXXXXXX,
+        XXXXXXX,
+
+        XXXXXXX,
+        KC_LGUI,
+        KC_LALT,
+        KC_LCTL,
+        KC_LSFT,
+        XXXXXXX,
+
+        XXXXXXX,
+        KC_RSFT,
+        KC_RCTL,
+        KC_RALT,
+        KC_RGUI,
+        XXXXXXX,
+
+        XXXXXXX,
+        DRGSCRL,
+        SNIPING,
+        _______,
+        EE_CLR,
+        QK_BOOT,
+
+        QK_BOOT,
+        EE_CLR,
+        _______,
+        SNIPING,
+        DRGSCRL,
+        XXXXXXX,
+
+        KC_BTN2,
+        KC_BTN1,
+        KC_BTN3,
+        
+        KC_BTN3,
+        KC_BTN1
   )
 };
 // clang-format on
