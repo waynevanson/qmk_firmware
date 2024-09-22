ebe3c21f16534d88448e6d007501179316fb8fb2
Wayne Van Son <=>
2024-09-22 11:57:12 +1000

    fix: hell yeah 5x3

--- keyboards/bastardkb/charybdis/3x6/config.h
+++ keyboards/bastardkb/charybdis/3x6/config.h
@@ -20,3 +20,22 @@
 
 /* Trackball angle adjustment. */
 #define ROTATIONAL_TRANSFORM_ANGLE -25
+
+/* Removes the outer columns and keyboard goes from left hand to right hand, rather than rows.*/
+#define LAYOUT_SPLIT(\
+    L01, L02, L03, L04, L05,\
+    L11, L12, L13, L14, L15,\
+    L21, L22, L23, L24, L25,\
+    L33, L34, L31,\
+    R00, R01, R02, R03, R04,\
+    R10, R11, R12, R13, R14,\
+    R20, R21, R22, R23, R24,\
+    R31, R33\
+)\
+LAYOUT(\
+    KC_NO, L01, L02, L03, L04, L05, R00, R01, R02, R03, R04, KC_NO,\
+    KC_NO, L11, L12, L13, L14, L15, R10, R11, R12, R13, R14, KC_NO,\
+    KC_NO, L21, L22, L23, L24, L25, R20, R21, R22, R23, R24, KC_NO,\
+    L33, L34, L31,\
+    R31, R33\
+)
--- keyboards/bastardkb/charybdis/3x6/keymaps/waynevanson/keymap.c
+++ keyboards/bastardkb/charybdis/3x6/keymaps/waynevanson/keymap.c
@@ -21,220 +21,61 @@
 
 // https://github.com/manna-harbour/miryoku/tree/master/docs/reference
 enum charybdis_keymap_layers {
-    // bByYoOuUzZ QqLlDdWwVv
-    // cCiIeEaA,; :.HhTtSsNn 
-    // gGxXjJkK'" !?RrMmFfPp
-    BASE = 0,
-    // Shift is F keys
-    // 12778899?? 
-    // 11445566
-    // 10112233
-    NMSY,
-    // What do I use the most?+=-&*()|`[]{}<>
-    // ??(<)>?^
-    // &|{[}]??
-    // ??[/]\??
-    //   ????-_
-    
-    FUNC,
-    MEDIA,
-    NAV,
-    LAYER_POINTER
+  LAYER_BASE = 0,
+  LAYER_NUMBER_FUNCTION,
+  LAYER_SYMBOLS,
+  LAYER_POINTER,
 };
 
 enum custom_keycodes {
-  KC_DOT_PIPE = SAFE_RANGE
+  KC_10 = SAFE_RANGE,
+  KC_11,
+  KC_12
 };
 
 const custom_shift_key_t custom_shift_keys[] = {
-  {KC_QUESTION, KC_EXCLAIM}, // Shift ? is !
-  {KC_COMM, KC_SCLN}, // Shift , is ;
-  {KC_DOT,  KC_COLN}, // Shift . is :   
+  // BASE
+  {KC_QUESTION, KC_EXCLAIM}, //  ? -> !
+  {KC_COMM, KC_SCLN}, // , -> ;
+  {KC_DOT,  KC_COLN}, // . -> :   
+  {LT(LAYER_NUMBER_FUNCTION, KC_BSPC), KC_DELETE}, // Backspace -> Delete
+
+  // NUM_FUN layer
+  {KC_1, KC_F1},
+  {KC_2, KC_F2},
+  {KC_3, KC_F3},
+  {KC_4, KC_F4},
+  {KC_5, KC_F5},
+  {KC_6, KC_F6},
+  {KC_7, KC_F7},
+  {KC_8, KC_F8},
+  {KC_9, KC_F9},
+  {KC_10, KC_F10},
+  {KC_11, KC_F11},
+  {KC_12, KC_F12},
+
   {KC_SLSH, KC_BSLS}, // Shift / is "\"
-  {LT(NMSY, KC_BSPC), KC_DELETE} // Shift Backspace is Delete
 };
 
 uint8_t NUM_CUSTOM_SHIFT_KEYS =
     sizeof(custom_shift_keys) / sizeof(custom_shift_key_t);
 
-// Symbols = 32
-// `~!@#$%^&*()-_=+[{]}\|;:'",<.>/?
-// Already used = 8  
-// .,:;'"?!
-// Remaining = 24
-//
-// 5 x 3 = 15
-// Nums Layer consumed = 11 
-// 1234567890.
-// Available 4 + 15 = 19
-// Looks like I need more than the number layout for this.
-
-// @#$%^&*()+=-_[{}]\/|<>`~ = 24
-// 3 + 3 + 2 = 8
-// 8 + 15 = 
-// MISSING ONE SPOT BECAUSE OF 0
-
-// ALternatively, I could put the function keys with the number layer on shift
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Engram layout (mostly) for letters (https://engram.dev/)
+  [LAYER_BASE] = LAYOUT_SPLIT(
+    KC_B,
+    KC_Y,
+    KC_O,
+    KC_U,
+    KC_Z,
 
-  [BASE] = LAYOUT(
-       KC_NO,
-       KC_B,
-       KC_Y,
-       KC_O,
-       KC_U,
-       KC_Z,
-       
-       KC_Q,
-       KC_L,
-       KC_D,
-       KC_W,
-       KC_V,
-       KC_Z,
-
-       KC_NO,
-       LGUI_T(KC_C),
-       LALT_T(KC_I),                
-       LCTL_T(KC_E),
-       LSFT_T(KC_A),
-       KC_COMM,
-
-       KC_DOT,
-       RSFT_T(KC_H),
-       RCTL_T(KC_T),
-       RALT_T(KC_S),
-       RGUI_T(KC_N),
-       KC_Q,  
-
-       KC_NO,
-       LT(LAYER_POINTER, KC_G),
-       KC_X,
-       KC_J,
-       KC_K,
-       KC_QUOT,
-
-       KC_QUESTION,
-       KC_R,
-       KC_M,
-       KC_F,
-       LT(LAYER_POINTER, KC_P),
-       KC_NO,
-
-       KC_ESC,
-       LT(NAV, KC_SPC),
-       KC_TAB,
-
-       LT(FUNC, KC_ENT),
-       LT(NMSY, KC_BSPC)
-  ),
-
-  [NMSY] = LAYOUT(
-        KC_NO,
-        KC_LBRC,
-        KC_7,
-        KC_8,
-        KC_9,
-        KC_RBRC,
-
-        KC_NO,
-        KC_NO,
-        KC_NO,
-        KC_NO,
-        KC_NO,
-        KC_NO,
-
-        KC_NO,
-        KC_SEMICOLON,
-        KC_4,
-        KC_5,
-        KC_6,
-        KC_EQL,
-
-        KC_NO,
-        KC_RSFT,
-        KC_RCTL,
-        KC_RALT,
-        KC_RGUI,
-        KC_NO,
-
-        KC_NO,
-        KC_GRAVE,
-        KC_1,
-        KC_2,
-        KC_3,
-        KC_SLSH,
-
-        KC_NO,
-        KC_NO,
-        KC_NO,
-        KC_NO,
-        KC_NO,
-        KC_NO,
-
-        KC_DOT_PIPE,
-        KC_0,
-        KC_MINS,
-
-        KC_NO,
-        KC_NO
-  ),
-
-  [FUNC] = LAYOUT(
-        KC_NO,
-        KC_F12,
-        KC_F7,
-        KC_F8,
-        KC_F9,
-        KC_PRINT_SCREEN,
-
-        KC_NO,
-        KC_NO,
-        KC_NO,
-        KC_NO,
-        KC_NO,
-        KC_NO,
-
-        KC_NO,
-        KC_F11,
-        KC_F4,
-        KC_F5,
-        KC_F6,
-        KC_SCROLL_LOCK,
-
-        KC_NO,
-        KC_RIGHT_SHIFT,
-        KC_RIGHT_CTRL,
-        KC_RIGHT_ALT,
-        KC_RIGHT_GUI,
-        KC_NO,
-
-        KC_NO,
-        KC_F11,
-        KC_F1,
-        KC_F2,
-        KC_F3,
-        KC_PAUSE,
-
-        KC_NO,
-        KC_NO,
-        KC_NO,
-        KC_NO,
-        KC_NO,
-        KC_NO,
-
-        KC_MENU,
-        KC_SPACE,
-        KC_TAB,
-
-        KC_NO,
-        KC_NO
-  ),
-
-  [NAV] = LAYOUT(
+    KC_Q,
+    KC_N,
     KC_NO,
+    KC_NO,
+    KC_NO,
+
     KC_NO,
     KC_NO,
     KC_NO,
@@ -242,50 +83,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,
 
     KC_NO,
-    KC_HOME,
-    KC_PGDN,
-    KC_PGUP,
-    KC_END,
-    KC_NO,
-
-    KC_NO,
-    KC_NO,
-    KC_NO,
-    KC_NO,
     KC_NO,
     KC_NO,
 
-    KC_CAPS,
-    KC_LEFT,
-    KC_DOWN,
-    KC_UP,
-    KC_RIGHT,
-    KC_NO,
-
-    KC_NO,
-    KC_NO,
-    KC_NO,
-    KC_NO,
-    KC_NO,
-    KC_NO,
-
-    KC_NO,
-    KC_MEDIA_PREV_TRACK,
-    KC_AUDIO_VOL_DOWN,
-    KC_AUDIO_VOL_UP,
-    KC_MEDIA_NEXT_TRACK,
-    KC_NO,
-
-    KC_NO,
-    KC_NO,
-    KC_NO,
-
-    KC_MEDIA_STOP,
-    KC_MEDIA_PLAY_PAUSE
-  ),
-
-  [LAYER_POINTER] = LAYOUT(
-    KC_NO,
     KC_NO,
     KC_NO,
     KC_NO,
@@ -297,69 +97,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,
     KC_NO,
     KC_NO,
-    KC_NO,
 
     KC_NO,
     KC_NO,
     KC_NO,
     KC_NO,
     KC_NO,
-    KC_NO,
-
-    KC_NO,
-    KC_NO,
-    KC_NO,
-    KC_NO,
-    KC_NO,
-    KC_NO,
-
-    KC_NO,
-    KC_NO,
-    KC_NO,
-    KC_NO,
-    KC_NO,
-    KC_NO,
-
-    KC_NO,
-    KC_NO,
-    KC_NO,
-    KC_NO,
-    KC_NO,
-    KC_NO,
-
-    KC_BTN2,
-    KC_BTN1,
-    KC_BTN3,
 
     KC_NO,
     KC_NO
-  ),
-
-  [LAYER_POINTER] = LAYOUT(
-  // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DPI_MOD, S_D_MOD,    S_D_MOD, DPI_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,    XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX,
-  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX, _______, DRGSCRL, SNIPING, EE_CLR,  QK_BOOT,    QK_BOOT, EE_CLR,  SNIPING, DRGSCRL, _______, XXXXXXX,
-  // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  KC_BTN2, KC_BTN1, KC_BTN3,    KC_NO, KC_NO
-  //                            ╰───────────────────────────╯ ╰──────────────────╯
   )
 };
 // clang-format on
 
-// todo: add  `>` and `<`
 void process_custom_keys(uint16_t keycode, keyrecord_t* record) {
-  switch (keycode) {
-    case KC_DOT_PIPE:
-      if (record->event.pressed) {
-        register_code(KC_DOT);
-      } else {
-        unregister_code(KC_DOT);
-      }
-    default:
-  };
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
